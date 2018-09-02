b84615a79547c0ae80b61c23949a2b91a0b3409f
Watt Erikson <turtle@turtle-coin.com>
2018-09-01 22:30:14 -0700

    Roto

---
+++ config.js
@@ -0,0 +1,19 @@
+var api = 'http://roto2coin.cf:13300';
+var donationAddress = "";
+var blockTargetInterval = 30;
+var coinUnits = 100;
+var symbol = 'ROTO';
+var refreshDelay = 30000;
+// pools stats by MainCoins
+var networkStat = {
+ "roto": [
+	["roto2coin.cf", "http://roto2coin.cf:8117"],
+	["pool.forovirgenes.es", "http://pool.forovirgenes.es:8117"]
+]
+};
+
+var networkStat2 = {
+    "roto": [
+	[""]
+ ]
+};
